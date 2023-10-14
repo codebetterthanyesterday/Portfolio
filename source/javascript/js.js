b645d5c83411dacf8cdb235bd318ddde784c50f8
codebetterthanyesterday <103586808+codebetterthanyesterday@users.noreply.github.com>
2023-10-13 20:39:16 -0700

    Update js.js

--- source/javascript/js.js
+++ source/javascript/js.js
@@ -131,6 +131,9 @@ var typed = new Typed(".typing", {
     var birthDate = new Date(tanggalLahir);
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
     return age;
 }
 
